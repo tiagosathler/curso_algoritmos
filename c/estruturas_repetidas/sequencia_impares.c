b266cd4180e64513b149d40500038d4329179881
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-08-22 15:30:19 -0300

    :sparkles: feat: conclui o exercício 'sequencia_impares.c'

---
+++ c/estruturas_repetidas/sequencia_impares.c
@@ -0,0 +1,24 @@
+/* Problema "sequencia_impares"
+ Leia um valor inteiro X.
+ Em seguida mostre os ímpares de 1 até X, um valor por linha, inclusive o X, se for o caso.
+*/
+
+#include <stdio.h>
+
+int main()
+{
+  int x;
+
+  printf("Digite o valor de X: ");
+  scanf("%d", &x);
+
+  for (int i = 1; i <= x; i++)
+  {
+    if (i % 2)
+    {
+      printf("%d\n", i);
+    }
+  }
+
+  return 0;
+}
