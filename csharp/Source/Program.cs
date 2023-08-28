a1adcee38c98a6c9246f9978370950616d9ec56c
Tiago Sathler <sathler@gmail.com>
2023-08-28 19:49:38 -0300

    :sparkles: feat: conclui o exercício 'Medidas.cs'

--- csharp/Source/Program.cs
+++ csharp/Source/Program.cs
@@ -14,6 +14,7 @@ internal static class Program
         /// Troco.Execute();
         /// Circulo.Execute();
         /// Pagamento.Execute();
-        Consumo.Execute();
+        /// Consumo.Execute();
+        Medidas.Execute();
     }
 }
