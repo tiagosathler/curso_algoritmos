f808c4dfaf5bded35ae826277e926ee06a386929
Tiago Heringer Siqueira Sathler <sathler@gmail.com>
2023-08-30 20:11:46 -0300

    :bug: fix: cancela o uso do arquivo principal 'main.py'

--- python/main.py
+++ python/main.py
@@ -3,8 +3,6 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-from src.estruturas.sequenciais.terreno import *
 
 if __name__ == '__main__':
-    # ESTRUTURAS SEQUENCIAIS:
-    terreno()
+    print("Hello World")
